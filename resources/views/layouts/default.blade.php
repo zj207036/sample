dabab07bc685a792fe2810959066faf14c9045db
Jzuo <zjaizyyy@163.com>
2018-04-04 07:53:31 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title','左俊的个人主页')</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+@extends('layouts.default')
+@section('title', '关于')
+@section('content')
+	<h1>关于页</h1>
+@stop
