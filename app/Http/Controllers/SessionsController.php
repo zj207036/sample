a409332475ba0e4426ca8dd998a9c9fb74b883a3
Jzuo <zjaizyyy@163.com>
2018-04-12 03:23:05 +0000

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request,[
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        if(Auth::attempt($credentials,$request->has('remember'))){
+            //登录成功之后的相关操作
+            session()->flash('success','欢迎回来');
+            return redirect()->route('users.show',[Auth::user()]);
+        } else {
+            //登录失败之后的相关操作
+            session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back();
+        }
+
+        return;
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已经成功退出');
+        return redirect('login');
+    }
+}
