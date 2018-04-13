3b1da08f6dd33dc627a26540560b1f6951b216aa
Jzuo <zjaizyyy@163.com>
2018-04-13 10:10:14 +0000

    添加一个login入口

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -27,7 +27,7 @@
             </li>
           @else
           <li><a href="{{ route('help') }}">帮助</a></li>
-          <li><a href="#">登录</a></li>
+          <li><a href="{{ route('login') }}">登录</a></li>
           @endif
         </ul>
       </nav>
