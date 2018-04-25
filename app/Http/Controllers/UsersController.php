1aa22508eeae9b77688cc537d030b4df3da7c4ad
Jzuo <zjaizyyy@163.com>
2018-04-25 07:15:17 +0000

    Finish user edit,update,list and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -79,8 +79,17 @@ class UsersController extends Controller
 
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(10);
+//        $users = User::all();
 
         return view('users.index',compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success','成功删除用户');
+        return back();
     }
 }
